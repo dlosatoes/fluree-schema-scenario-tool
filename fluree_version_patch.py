287e54381b6b6ac0ec3e5fe824bf78e538a954da
Rob Meijer <rob@noemail>
2022-09-14 18:11:06 +0200

    updated to newest version of aioflureedb with domainapi explicitly enabled

--- fluree_version_patch.py
+++ fluree_version_patch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from pyparsing import nestedExpr, LineEnd
 
-VERSION = "0.4.8"
+VERSION = "0.5.0"
 
 def remove_comments(data):
     rval = list()
--- make_docker.sh
+++ make_docker.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#echo "Making from downloaded stable"
-#docker build --no-cache -t pibara/fsst:stable .
-#echo -n STABLE = 
-#docker run -it pibara/fsst:stable find /usr/src -type d|grep fluree|sed -e 's/.*\///' -e 's/\r//'
-#docker tag pibara/fsst:stable pibara/fsst:`docker run -it pibara/fsst:stable find /usr/src -type d|grep fluree|sed -e 's/.*\///' -e 's/\r//'`
-#docker tag pibara/fsst:stable pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`
-#docker tag pibara/fsst:stable pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-stable
-#docker tag pibara/fsst:stable pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-`docker run -it pibara/fsst:stable find /usr/src -type d|grep fluree|sed -e 's/.*\///' -e 's/\r//'`
-#echo
 echo "Making latest beta/stable from github"
 echo -n BETA =
 python3 git_info_fluree_latest.py
@@ -17,11 +8,13 @@ docker tag pibara/fsst:beta pibara/fsst:`python3 git_info_fluree_latest.py | sed
 docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-`python3 git_info_fluree_latest.py | sed -e 's/\r//'`
 docker tag pibara/fsst:beta pibara/fsst:stable
 docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-stable
-echo "Making latest alpha from github"
-echo -n BETA =
-python3 git_info_fluree_latest_alpha.py
-docker build --no-cache -t pibara/fsst:alpha . -f Dockerfile-alpha-github
-docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-alpha
-docker tag pibara/fsst:beta pibara/fsst:`python3 git_info_fluree_latest_alpha.py | sed -e 's/\r//'`
-docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-`python3 git_info_fluree_latest_alpha.py | sed -e 's/\r//'`
+echo "Making 1.0.0"
+
+#echo "Making latest alpha from github"
+#echo -n BETA =
+#python3 git_info_fluree_latest_alpha.py
+#docker build --no-cache -t pibara/fsst:alpha . -f Dockerfile-alpha-github
+#docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-alpha
+#docker tag pibara/fsst:beta pibara/fsst:`python3 git_info_fluree_latest_alpha.py | sed -e 's/\r//'`
+#docker tag pibara/fsst:beta pibara/fsst:v`grep "VERSION.*=" fsst | sed -e 's/.*= *"//' -e 's/"//'`-`python3 git_info_fluree_latest_alpha.py | sed -e 's/\r//'`
 
